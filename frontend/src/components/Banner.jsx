6ccc1aaf87801e5a2e8e525ac5e86795d238310f
shweta1210 <shwetasingh1545@gmail.com>
2024-08-09 13:22:37 +0530

    Designing home page

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import image from '../assets/image/swapsangambanner.jpg'
+function Banner() {
+  return (
+   <>
+    <div className=' max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row'>
+        <div className=' order-2 md:order-1 w-1/2 mt-10 md:mt-24'>
+            <div className='space-y-9'>
+            <h1 className='text-4xl font-bold'>Hello! Welcome to <spam className="text-purple-800">SwapSangam</spam></h1>
+            <p className='font-semibold'>We welcome you to the exclusive MNNIT mart where you can buy and sell with convenience</p>
+            <button className="btn bg-purple-800 hover:bg-purple-800/75">Get Started</button>
+            </div>
+        </div>
+        <div className='order-1 w-1/2 mt-10 md:mt-24 ml-10'>
+          <img src={image} className=''></img>
+        </div>
+    </div>
+   </>
+  )
+}
+
+export default Banner
